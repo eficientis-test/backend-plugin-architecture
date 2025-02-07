4b74b01a4a63d721ac95fe1b3a3c87a4c44bf4b7
Guillermo Palacios <nothicc04@gmail.com>
2025-02-07 14:56:38 -0500

    v1.0.4-core

---
+++ backend/functions/src/core/index.ts
@@ -0,0 +1,7 @@
+export * from "./EventBus";
+export * from "./FirebaseApp";
+export * from "./GraphQLServer";
+export * from "./Kernel";
+export * from "./PluginContract";
+export * from "./PluginManager";
+export * from "./PluginRegistry";
